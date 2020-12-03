de7832ba6b673218303cac64125bee19ca333f6f
fpignataro <>
2020-12-03 15:02:36 -0300

    quito labels

--- main.tf
+++ main.tf
@@ -3,23 +3,12 @@
 ## Copyright @ CloudDrove. All Right Reserved.
 
 
-#Module      : LABEL
-#Description : Terraform label module variables.
-module "labels" {
-  source      = "clouddrove/labels/digitalocean"
-  version     = "0.13.0"
-  name        = var.name
-  application = var.application
-  environment = var.environment
-  label_order = var.label_order
-}
-
 #Module      : VPC
 #Description : VPCs are virtual networks containing resources that can communicate with each other in full isolation, using private IP addresses.
 resource "digitalocean_vpc" "default" {
   count = var.enable_vpc == true ? 1 : 0
 
-  name        = module.labels.id
+  name        = var.name
   region      = var.region
   description = var.description
   ip_range    = var.ip_ragne
